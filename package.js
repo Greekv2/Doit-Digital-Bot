21a1d763f8e94e745f1e53523fd4b50b1553eebb
Greekv2 <rengine322@gmail.com>
2025-01-24 02:07:09 +0200

    package.js

---
+++ package.js
@@ -0,0 +1,118 @@
+{
+    "status": {
+        "accountactivity": "Rangers RolePlay /",
+        "link": "https://www.twitch.tv/GKan#8537",
+        "status": "Rangers RolePlay",
+        "type": "PLAYNING"
+    },
+    "other": {
+        "prefix": "!",
+        "invitelink": "https://discord.gg/NS4dFj253R",
+        "serverName": "Rangers RolePlay",
+        "serverdev": "carolosjk and matza",
+        "serverimage": "https://media.discordapp.net/attachments/957646662749278219/1200161986831462400/tx2.png?ex=65c52cf3&is=65b2b7f3&hm=5839113c68e5dd860bb69b7e14b35c68f31e6fd8caffafc279a7559741cedef6&=&format=webp&quality=lossless"
+    },
+    "ServerCount": {
+        "GUILD-ID": "1315308244859748423",
+        "MEMBERS": "1195478935555805335",
+        "BOOSTS": "1195478938592493680"
+    },
+    "ticket": {
+        "message id": "",
+        "ERid": "1126452244456419429",
+        "AccessID": "1195478565714669630",
+        "Donid": "1195478405483864175",
+        "logs": "1195478948872724580"
+    },
+    "reports": {
+        "bug": "",
+        "buglogs": "",
+        "suggestions": "",
+        "spoils": "",
+        "pols": ""
+    },
+    "rules": {
+        "general": "1195479228750254110",
+        "zones": "",
+        "events": "",
+        "rob": "1195479235284975678",
+        "criminal": "1195479238929809420",
+        "police": "1195479232168595547",
+        "ambulance": "1195479243036045413",
+        "mafia": "",
+        "hooligan": "",
+        "reportSys": ""
+    },
+    "donate": {
+        "newpacks": "1178080311075151924",
+        "combos": "1178080317236596767",
+        "vipacks": "1178080321430888474", 
+        "oilrings": "1178080325021225066",
+        "doncars": "1178080332885541045",
+        "extraspacks": "1178080336370995260",
+        "agreements": "1178080340233965639",
+        "job": "1178080343627149323"
+    },
+    "act": {
+        "onduty": "1195669245669941370",
+        "logs": "1195478945240465578"
+    },
+    "logs": {
+        "join": "1195478941671112736",
+        "leave": "1195478941671112736",
+        "ban": "1195478956216946758",
+        "unban": "1195478956216946758",
+        "messages": "1195478962726518915",
+        "voice": "1195478959488512011",
+        "userrole": "1195478952827949076",
+        "member": "1195478941671112736",
+        "rolecreate": "1195478952827949076",
+        "channel": "1195478972272754868",
+        "alts": "1195478975577870588",
+        "antilink": "1195478980170620978",
+        "startbot": "1195478980170620978",
+        "staffAppLogs": "1195478987649077341"
+    },
+    "applications": {
+        "Staff": "https://forms.gle/brrByEwwkd6MmRmG9",
+        "Ekab": "https://forms.gle/UkcsmdUccDAhYtCw8",
+        "Astynomia": "https://forms.gle/xPigdWqvHxmK5eYp6",
+        "Stratos" : "https://forms.gle/czv7Dp5VybNuAKns8",
+        "Limeniko" : "https://forms.gle/Zne9adDs2W1dxAgKA"
+    },
+    "WFSCHANNEL": "1195479183405613057",
+    "WFSCATEGORY": "1195478915100180490",
+
+    "WFICHANNEL": "1195479186882703390",
+    "WFICATEGORY": "1195478915100180490",
+
+    "STAFFMANAGERROLE": "1195478443584934071",
+
+    "STAFFROLE": "1195478565714669630",
+
+    "donatenot": "1195479190141673602",
+    "supportnot": "1195479180066955435",
+
+    "share": "1195478853120970752",
+    "EMBEDCOLOR": "#284c94",
+    "access": "1195478565714669630",
+
+    "ESX": "1200171966586425434",
+    "QBCORE": "1200172034521579540",
+    "PVP": "1200172139014266951",
+
+    "DONATEMANAGER": "1195478405483864175",
+    "WFDCHANNEL": "1195479193333530734",
+    "WFDCATEGORY": "1195478918426267800",
+    "staff team role id": "1195478565714669630",
+    "CIVILIANROLE": "1195478843885109320",
+    "fivem": {
+        "ip": "",
+        "port": ""
+    },
+    "livestatus": {
+        "channel": "1178080409452564590",
+        "online": "🟢",
+        "offline": "🔴"
+    }
+}
